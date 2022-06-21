301eb84166b74b77ac5d41ef0d4677a3788f812b
Paolo ferrari <paoloferrariapple@gmail.com>
2022-06-21 15:23:26 +0200

    Initial Commit

---
+++ Fishes/FishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishesApp.swift
+//  Fishes
+//
+//  Created by user on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
